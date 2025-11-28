0111e1c79976e21725d7b4c3689ca1e373c77497
Cedric <83550653+cedi138@users.noreply.github.com>
2025-11-28 18:07:23 +0100

    Create ergebnisse.js

---
+++ ergebnisse.js
@@ -0,0 +1,17 @@
+// ======= Gruppen & Teams =======
+
+const gruppen = {
+    A: ["Team 1", "Team 2", "Team 3", "Team 4"],
+    B: ["Team 5", "Team 6", "Team 7", "Team 8"],
+    C: ["Team 9", "Team 10", "Team 11", "Team 12"]
+};
+
+// ======= Ergebnisliste =======
+// Du trägst hier die SPIELERGEBNISSE ein!
+
+// Beispiel (löschen oder verändern):
+// { gruppe:"A", team1:"Team 1", team2:"Team 2", score1:3, score2:1 },
+
+const ergebnisse = [
+    // Noch leer — Tabelle wird trotzdem angezeigt!
+];
