92589caf2f7dc020d5ba370ebd5e7d7d0eeecd9a
Cedric <83550653+cedi138@users.noreply.github.com>
2025-12-03 23:30:22 +0100

    Update ergebnisse.js

--- ergebnisse.js
+++ ergebnisse.js
@@ -35,7 +35,7 @@ function getTeam(gruppe, index) {
 // Zeiten in 6 Spielblöcken: 19:15, 19:30, 19:45, 20:00, 20:15, 20:30
 // Pro Block: 3 Spiele – Tisch 1 (A), Tisch 2 (B), Tisch 3 (C)
 
-const time = ["19:15", "19:30", "19:45", "20:00", "20:15", "23:15"]
+const time = ["19:15", "19:30", "19:45", "20:00", "20:15", "23:30"]
 const spiele = [
   // Block 1 – 19:15
   { zeit: time[0], tisch: 1, gruppe: "A", teamA: getTeam("A", 0), teamB: getTeam("A", 1), ergebnis: "6:4" },
@@ -43,9 +43,9 @@ const spiele = [
   { zeit: time[0], tisch: 3, gruppe: "C", teamA: getTeam("C", 0), teamB: getTeam("C", 1), ergebnis: "2:3" },
 
   // Block 2 – 19:30
-  { zeit: time[1], tisch: 1, gruppe: "A", teamA: getTeam("A", 2), teamB: getTeam("A", 3), ergebnis: "2:2" },
-  { zeit: time[1], tisch: 2, gruppe: "B", teamA: getTeam("B", 2), teamB: getTeam("B", 3), ergebnis: "4:6" },
-  { zeit: time[1], tisch: 3, gruppe: "C", teamA: getTeam("C", 2), teamB: getTeam("C", 3), ergebnis: "1:3" },
+  { zeit: time[1], tisch: 1, gruppe: "A", teamA: getTeam("A", 2), teamB: getTeam("A", 3), ergebnis: "" },
+  { zeit: time[1], tisch: 2, gruppe: "B", teamA: getTeam("B", 2), teamB: getTeam("B", 3), ergebnis: "" },
+  { zeit: time[1], tisch: 3, gruppe: "C", teamA: getTeam("C", 2), teamB: getTeam("C", 3), ergebnis: "" },
 
   // Block 3 – 19:45
   { zeit: time[2], tisch: 1, gruppe: "A", teamA: getTeam("A", 0), teamB: getTeam("A", 2), ergebnis: "" },
