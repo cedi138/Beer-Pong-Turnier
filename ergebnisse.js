23b33576219b284bef47eab602260694b5857e4c
Cedric <83550653+cedi138@users.noreply.github.com>
2025-12-04 14:55:30 +0100

    Update ergebnisse.js

--- ergebnisse.js
+++ ergebnisse.js
@@ -1,35 +1,3 @@
-// TEAMS + SPIELER
-const teams = {
-  A: [
-    { name: "Team 1", members: ["Teilnehmer 1", "Teilnehmer 2", "Teilnehmer 3"] },
-    { name: "Team 2", members: ["Teilnehmer 1", "Teilnehmer 2"] },
-    { name: "Team 3", members: ["Teilnehmer 1", "Teilnehmer 2", "Teilnehmer 3"] },
-    { name: "Team 4", members: ["Teilnehmer 1", "Teilnehmer 2"] }
-  ],
-
-  B: [
-    { name: "Team 5", members: ["Teilnehmer 1", "Teilnehmer 2", "Teilnehmer 3"] },
-    { name: "Team 6", members: ["Teilnehmer 1", "Teilnehmer 2"] },
-    { name: "Team 7", members: ["Teilnehmer 1", "Teilnehmer 2", "Teilnehmer 3"] },
-    { name: "Team 8", members: ["Teilnehmer 1", "Teilnehmer 2"] }
-  ],
-
-  C: [
-    { name: "Team 9", members: ["Teilnehmer 1", "Teilnehmer 2", "Teilnehmer 3"] },
-    { name: "Team 10", members: ["Teilnehmer 1", "Teilnehmer 2"] },
-    { name: "Team 11", members: ["Teilnehmer 1", "Teilnehmer 2", "Teilnehmer 3"] },
-    { name: "Team 12", members: ["Teilnehmer 1", "Teilnehmer 2"] }
-  ]
-};
-
-// ---------------------------------------------------
-// HELFER: Team-Name per Index
-// ---------------------------------------------------
-function getTeam(gruppe, index) {
-  return teams[gruppe][index].name;
-}
-
-
 // --- Spielplan ---------------------------------------------------
 // Jede Gruppe: Round-Robin (jeder-gegen-jeden)
 // Zeiten in 6 Spielblöcken: 19:15, 19:30, 19:45, 20:00, 20:15, 20:30
