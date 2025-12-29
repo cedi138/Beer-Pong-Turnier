872c8d2add47d3eea0d96733dbc89a66e5001752
Cedric <83550653+cedi138@users.noreply.github.com>
2025-12-29 19:30:41 +0100

    Update ergebnisse.js

--- ergebnisse.js
+++ ergebnisse.js
@@ -6,9 +6,9 @@
 const time = ["19:20", "19:45", "20:00", "20:15", "20:30", "20:45", "21:00", "21:15", "21:30", "21:45", "22:00"]
 const spiele = [
   // Block 1 – 19:15
-  { zeit: time[0], tisch: 1, gruppe: "A", teamA: getTeam("A", 0), teamB: getTeam("A", 1), ergebnis: "" },    //Game 1
-  { zeit: time[0], tisch: 2, gruppe: "B", teamA: getTeam("B", 0), teamB: getTeam("B", 1), ergebnis: "" },
-  { zeit: time[0], tisch: 3, gruppe: "C", teamA: getTeam("C", 0), teamB: getTeam("C", 1), ergebnis: "" },
+  { zeit: time[0], tisch: 1, gruppe: "A", teamA: getTeam("A", 0), teamB: getTeam("A", 1), ergebnis: "4:6" },    //Game 1
+  { zeit: time[0], tisch: 2, gruppe: "B", teamA: getTeam("B", 0), teamB: getTeam("B", 1), ergebnis: "6:5" },
+  { zeit: time[0], tisch: 3, gruppe: "C", teamA: getTeam("C", 0), teamB: getTeam("C", 1), ergebnis: "3:6" },
 
   // Block 2 – 19:30
   { zeit: time[1], tisch: 1, gruppe: "A", teamA: getTeam("A", 2), teamB: getTeam("A", 3), ergebnis: "" },    //Game 4
